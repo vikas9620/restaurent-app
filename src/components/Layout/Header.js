2827e68f2c3d7bab04ad70e8db653d43420a9d5e
vikas9620 <53708079+vikas9620@users.noreply.github.com>
2023-05-20 21:06:41 +0530

    Cart open and close

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,11 +4,12 @@ import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
+
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
         
       </header>
      
